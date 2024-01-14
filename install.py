63fd105bdf1bafb48eaf6d9a9d6988a4e037f602
safubuki <safubuki.dev@gmail.com>
2024-01-14 19:16:30 +0900

    delete install py

--- install.py
+++
@@ -1,9 +0,0 @@
-# import launch
-
-# NOTE: This file is not used in this extension, but kept for future reference.(by safu)
-#       If you want to use this file, please remove the comment out.
-
-# TODO: add pip dependency if need extra module only on extension
-
-# if not launch.is_installed("aitextgen"):
-#     launch.run_pip("install aitextgen==0.6.0", "requirements for MagicPrompt")
